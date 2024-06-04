f8224604f57b19d4cbd4570ddc8188e3168c4ae4
somnathraz <somnathkhadanga810@gmail.com>
2024-06-04 15:02:39 +0530

    frontend setup done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { Nunito_Sans } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+const Nunito = Nunito_Sans({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={Nunito.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
