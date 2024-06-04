e9dadf85164c03c1ece40615c5196c9159589b2a
Rohit Gupta <gshaily45@gmail.com>
2024-06-04 16:57:55 +0530

    start

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "./Navbar.module.css";
+import Image from "next/image";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.navWrapper}>
+      <h2>Navbar</h2>
+      <Image
+        src="https://outspokn-fr.s3.us-east-2.amazonaws.com/Outspokn-web/Outspokn-logo-new.png"
+        alt="outspoknlogo"
+        width={330}
+        height={66}
+      />
+    </div>
+  );
 };
 
 export default Navbar;
